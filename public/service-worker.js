64f154b7ebd8027e1632a22fc7e5634cee231e99
Vincent <thabisov25@gmail.com>
2024-10-04 11:39:57 +0200

    Fixed routing with HashRouter

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,4 @@
-// Service Worker
-
-const CACHE_NAME = "vincent-pt-cache";
+const CACHE_NAME = "vincent-pt-cache-v2"; // Update version number when changing files
 const urlsToCache = [
   "/",
   "/index.html",
@@ -14,6 +12,22 @@ self.addEventListener("install", function(event) {
     caches.open(CACHE_NAME).then(function(cache) {
       console.log("Opened cache");
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Remove old caches during activation
+self.addEventListener("activate", function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
